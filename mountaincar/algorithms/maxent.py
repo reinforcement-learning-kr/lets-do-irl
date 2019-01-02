c3024ce516178e8bc776b1ef224621aa6a01ccae
dongminleeai <kid33629@gmail.com>
2019-01-03 01:02:44 +0900

    Modifying

--- mountaincar/algorithms/maxent.py
+++ mountaincar/algorithms/maxent.py
@@ -1,63 +1,62 @@
 import numpy as np
 from itertools import product
-import maxent_q_learning
+from maxent_q_learning import find_policy
 
-def maxent_irl(grid_states, n_actions, q_table, gamma, 
-                trajectories, epochs, learning_rate, env):
+def maxent_irl(feature_matrix, n_actions, gamma, 
+                trajectories, epochs, learning_rate):
+    # n_states : 2500
+    n_states = feature_matrix.shape[0]
+    
     # Initialise weights.
-    theta = np.random.uniform(size=(grid_states,))
+    theta = np.random.uniform(size=(n_states,))
     
     # Calculate the feature expectations \tilde{f}. 
-    feature_expectations = find_feature_expectations(grid_states, trajectories)
+    feature_expectations = find_feature_expectations(feature_matrix, trajectories)
 
     # Gradient descent on theta.
     for i in range(epochs):
-        reward = np.zeros((grid_states, n_actions, grid_states))
+        reward = feature_matrix.dot(theta)
 
-        expected_svf = find_expected_svf(grid_states, n_actions, reward, gamma, q_table, trajectories)
-        gradient = feature_expectations - grid_states.T.dot(expected_svf)
-        reward += learning_rate * gradient
-    return reward # (2500, 3, 2500)
+        expected_svf = find_expected_svf(n_states, n_actions, reward, gamma, trajectories)
+        gradient = feature_expectations - feature_matrix.T.dot(expected_svf)
+        theta += learning_rate * gradient
+    return feature_matrix.dot(theta).reshape((n_states,)) # (2500,)
 
 
-def find_feature_expectations(grid_states, trajectories):
+def find_feature_expectations(feature_matrix, trajectories):
     """ Calculate the feature expectations \tilde{f}. """
-    feature_expectations = np.zeros(grid_states.shape)
+    feature_expectations = np.zeros(feature_matrix.shape[1])
     
-    # trajectories = (20, 100, 4)
     for trajectory in trajectories:
-        for position, velocity, _, _ in trajectory:
-            state = [position, velocity]
-            state = maxent_q_learning.idx_to_state(env, state)
-            feature_expectations += grid_states[state]
+        for state_idx, _, _ in trajectory:
+            feature_expectations += feature_matrix[state_idx]
 
     feature_expectations /= trajectories.shape[0]
     return feature_expectations
 
 
-def find_expected_svf(grid_states, n_actions, reward, gamma, policy, trajectories):
+def find_expected_svf(n_states, n_actions, reward, gamma, trajectories):
     """ Algorithm 1, Expected Edge Frequency Calculation """
     n_trajectories = trajectories.shape[0] # 20
     trajectory_length = trajectories.shape[1] # 100
     
     # Step 3 in Local action probability computation & Step 4 in Forward pass 
-    start_state_count = np.zeros((n_states, n_states))
+    start_state_count = np.zeros(n_states)
+
     for trajectory in trajectories:
-        state = [trajectory[0][0], trajectory[0][1]]
-        position_idx, velocity_idx = q_learning.idx_to_state(env, state, n_states)
-        start_state_count[position_idx, velocity_idx] += 1
+        start_state_count[trajectory[0, 0]] += 1
     p_initial_state = start_state_count/n_trajectories
     
     expected_svf = np.tile(p_initial_state, (trajectory_length, 1)).T
 
-    policy = maxent_q_learning.find_policy()
+    # (2500, 3)
+    policy = find_policy()
 
     # Step 5 in Forward pass
     for t in range(1, trajectory_length):
         expected_svf[:, t] = 0
         for i, j, k in product(range(n_states), range(n_actions), range(n_states)):
-            expected_svf[k, t] += (expected_svf[i, t-1] *
-                                  policy[i, j] # Stochastic policy
+            expected_svf[k, t] += expected_svf[i, t-1] * policy[i, j] # Stochastic policy
     
     # Step 6 in Summi􏰁ng frequencies
     return expected_svf.sum(axis=1)
--- mountaincar/maxent_q_learning.py
+++ mountaincar/maxent_q_learning.py
@@ -4,7 +4,7 @@ import numpy as np
 import make_expert
 from algorithms import maxent
 
-n_states = 2500 
+n_states = 2500 # position - 50, velocity - 50
 n_actions = 3
 
 feature_matrix = np.eye((n_states)) # (2500, 2500)
@@ -12,19 +12,18 @@ q_table = np.zeros((n_states, n_actions)) # (2500, 3)
 
 gamma = 0.9
 q_learning_rate = 0.03
-epochs = 300
+epochs = 200
 theta_learning_rate = 0.01
 
-# (20, 100, 4)
 trejectories = np.load(file="make_expert/expert_trajectories.npy")
 
 def idx_to_state(env, state):
-    env_low = env.observation_space.low # [-1.2  -0.07]
-    env_high = env.observation_space.high # [0.6  0.07]
+    env_low = env.observation_space.low
+    env_high = env.observation_space.high 
     env_distance = (env_high - env_low) / 50 
     position_idx = int((state[0] - env_low[0]) / env_distance[0])
     velocity_idx = int((state[1] - env_low[1]) / env_distance[1])
-    state_idx = position_idx + velocity_idx*50
+    state_idx = position_idx + velocity_idx * 50
     return state_idx
 
 def get_action(state_idx):
@@ -52,10 +51,10 @@ def main():
             action = get_action(q_table[state_idx])
             next_state, _, done, _ = env.step(action)
             
-            # (2500,)
-            irl_reward = maxent.maxent_irl(grid_states, n_actions, gamma, 
-                                trajectories, epochs, theta_learning_rate, env)         
+            # (2500,)        
             next_state_idx = idx_to_state(env, next_state)                 
+            irl_reward = maxent.maxent_irl(feature_matrix, n_actions, gamma, 
+                                            trajectories, epochs, theta_learning_rate)
             reward = irl_reward[next_state_idx]
             update_q_table(state, action, reward, next_state)
 
