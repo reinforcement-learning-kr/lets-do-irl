30f47ba1af41801df2ea73178c13d5149dd45a31
Geonhee-LEE <gunhee6392@gmail.com>
2019-01-03 20:09:20 +0900

    init

--- mountaincar/algorithms/maxent.py
+++ mountaincar/algorithms/maxent.py
@@ -1,60 +1,91 @@
 import numpy as np
 from itertools import product
-import q_learning
+import numpy.random as rn
 
-def maxent_irl(grid_states, n_actions, q_table, gamma, 
+import maxent_q_learning
+
+def maxent_irl(feature_matrix, n_actions, q_table, gamma, 
                 trajectories, epochs, learning_rate, env):
-    reward = np.zeros((grid_states, n_actions, grid_states))
-    n_states = feature_matrix.shape[0]
+    print ("########## maxent_irl(feature_matrix, n_actions, q_table, gamma,trajectories, epochs, learning_rate, env) ##########")
     
+    print ("### feature_matrix:", feature_matrix)
+    print ("### n_actions:", n_actions)
+    
+    n_states, d_states = feature_matrix.shape
+    print ("### n_states:", n_states)
+    print ("### d_states:", d_states)
+
+
+    # Initialise weights.
+    theta = np.random.uniform(size=(n_states,))
+    print ("### theta:", theta)
+        
+    print ("### trajectories:", trajectories)
     # Calculate the feature expectations \tilde{f}. 
-    feature_expectations = find_feature_expectations(feature_matrix, trajectories)
+    feature_expectations = find_feature_expectations(feature_matrix, trajectories, env)
+    print ("### feature_expectations:", feature_expectations)
 
     # Gradient descent on theta.
     for i in range(epochs):
-        reward = feature_matrix.dot(theta)
+        theta = feature_matrix.dot(theta)
 
-        expected_svf = find_expected_svf(n_states, n_actions, reward, gamma, q_table, trajectories)
+        expected_svf = find_expected_svf(n_states, n_actions, theta, gamma, trajectories)
         gradient = feature_expectations - feature_matrix.T.dot(expected_svf)
-        reward += learning_rate * gradient
-    return reward # (2500, 3, 2500)
+        theta += learning_rate * gradient
+    return theta # (2500, 3, 2500)
+    print ("########## maxent_irl(grid_states, n_actions, q_table, gamma,trajectories, epochs, learning_rate, env) ##########")
+    
 
 
-def find_feature_expectations(feature_matrix, trajectories):
+def find_feature_expectations(feature_matrix, trajectories, env):
+    print ("##########find_feature_expectations(feature_matrix, trajectories, env) ##########")
+    
     """ Calculate the feature expectations \tilde{f}. """
-    feature_expectations = np.zeros(feature_matrix.shape[0])
+    feature_expectations = np.zeros(feature_matrix.shape[1])
+    print ("feature_expectations", feature_expectations)
     
     # trajectories = (20, 100, 4)
     for trajectory in trajectories:
-        for state, _, _ in trajectory: # state, action, reward
-            idx_to_state
-            feature_expectations += feature_matrix[state]
+        #print ("trajectory", trajectory)
+        for state_idx, _, _ in trajectory: # state, action, reward
+            feature_expectations += feature_matrix[int(state_idx)]
 
     feature_expectations /= trajectories.shape[0]
+    print ("##########find_feature_expectations(feature_matrix, trajectories, env) ##########")
     return feature_expectations
 
 
-def find_expected_svf(n_states, n_actions, reward, gamma, policy, trajectories):
+def find_expected_svf(n_states, n_actions, reward, gamma, trajectories):
+    print ("##########find_expected_svf(n_states, n_actions, reward, gamma, trajectories) ##########")
     """ Algorithm 1, Expected Edge Frequency Calculation """
     n_trajectories = trajectories.shape[0] # 20
     trajectory_length = trajectories.shape[1] # 100
+
     
-    # Step 3 in Local action probability computation & Step 4 in Forward pass 
-    start_state_count = np.zeros((n_states, n_states))
+    
+    # Step 3 in Local action probability computation 
+    # & Step 4 in Forward pass 
+    start_state_count = np.zeros(n_states)
+
     for trajectory in trajectories:
-        state = [trajectory[0][0], trajectory[0][1]]
-        position_idx, velocity_idx = q_learning.idx_to_state(env, state, n_states)
-        start_state_count[position_idx, velocity_idx] += 1
+        print("trajectory[0, 0]:", trajectory[0, 0])
+        start_state_count[int(trajectory[0, 0])] += 1
     p_initial_state = start_state_count/n_trajectories
     
     expected_svf = np.tile(p_initial_state, (trajectory_length, 1)).T
+
+    # (2500, 3)
+    policy = maxent_q_learning.find_policy()
 
     # Step 5 in Forward pass
     for t in range(1, trajectory_length):
         expected_svf[:, t] = 0
         for i, j, k in product(range(n_states), range(n_actions), range(n_states)):
-            expected_svf[k, t] += (expected_svf[i, t-1] *
-                                  policy[i, j] # Stochastic policy
+            print("[t][i][j][k]:", t, i, j, k)
+            print("expected_svf[k, t]:", expected_svf[k, t])
+            expected_svf[k, t] += expected_svf[i, t-1] * policy[i, j] # Stochastic policy
     
     # Step 6 in Summi􏰁ng frequencies
+    print ("##########find_expected_svf(n_states, n_actions, reward, gamma, trajectories) ##########")
+    
     return expected_svf.sum(axis=1)
--- mountaincar/maxent_q_learning.py
+++ mountaincar/maxent_q_learning.py
@@ -1,63 +1,141 @@
 import gym
 import numpy as np
 
-import make_expert
 from algorithms import maxent
 
-n_states = 50 
-n_actions = 3
+pos_vel_state = 50
+n_states = 2500 # position - 50, velocity - 50
+n_actions = 3 # Left, Stop, Right
 
-grid_states = np.zeros((n_states**2)) # (2500,)
-q_table = np.zeros((n_states**2, n_actions)) # (2500, 3)
+feature_matrix = np.eye((n_states)) # (2500, 2500)
+
+# grid_State with 3 action types
+q_table = np.zeros((n_states**2, n_actions)) # (2500, 3) 
+
 
 gamma = 0.9
 q_learning_rate = 0.03
 epochs = 300
 theta_learning_rate = 0.01
 
-# (20, 100, 4)
-trejectories = np.load(file="make_expert/expert_trajectories.npy")
+def conv_traj(env, pos_vel_state):
+    print ("########## conv_traj(env, pos_vel_state ##########")
+    #global trajectories
+    env_low = env.observation_space.low     # [-1.2, -0.07]
+    env_high = env.observation_space.high   # [0.6, 0.07]
+    env_distance = (env_high - env_low) / pos_vel_state  # n_state = 50
 
-def idx_to_state(env, state, n_states):
-    env_low = env.observation_space.low 
-    env_high = env.observation_space.high 
-    env_distance = (env_high - env_low) / n_states 
+    raw_trej = np.load(file="make_expert/expert_trajectories.npy")
+    
+    '''
+    print ("### trejectories:", raw_trej)   # (state[0], state[1], action, reward) ((20, 100, 4))
+    print ("### trejectories :", raw_trej[0][0][0], raw_trej[0][0][1])
+    print ("### trejectories :", len(raw_trej[0][0]))
+    print ("### trejectories :", len(raw_trej[0]))
+    print ("### trejectories :", len(raw_trej))
+    #print ("### trejectories shape:", raw_trej[1])
+    '''
+    
+    trajectories = np.zeros((len(raw_trej), len(raw_trej[0]), 3))
+
+    for x in range(0, len(raw_trej)):
+        for y in range(0, len(raw_trej[0])):
+            position_idx = int((raw_trej[x][y][0] - env_low[0]) / env_distance[0])
+            velocity_idx = int((raw_trej[x][y][1] - env_low[1]) / env_distance[1])
+            state_idx = position_idx + velocity_idx*pos_vel_state
+
+            trajectories[x][y][0] = state_idx
+            trajectories[x][y][1] = raw_trej[x][y][2] 
+            trajectories[x][y][2] = raw_trej[x][y][3] 
+            '''
+            print("[x],[y]:", x ,y)
+            print("state_idx:", state_idx)
+            print("raw_trej[x][y][0],raw_trej[x][y][1]:", raw_trej[x][y][0] ,raw_trej[x][y][1])
+            print("trajectories[x][y]:", trajectories[x][y])
+
+    
+    print ("### trejectories :", trajectories[0][0][0], trajectories[0][0][1])
+    print ("### trejectories :", len(trajectories[0][0]))
+    print ("### trejectories :", len(trajectories[0]))
+    print ("### trejectories :", len(trajectories))
+    print ("### type(trejectories) :", (trajectories.shape))
+    
+
+    print (trajectories)
+    '''
+    print ("########## conv_traj(env, pos_vel_state ##########")
+    return trajectories
+
+def find_policy():
+    return q_table
+
+    
+def idx_to_state(env, state):
+    print ("########## idx_to_state(env, state) ##########")
+    env_low = env.observation_space.low     # [-1.2, -0.07]
+    env_high = env.observation_space.high   # [0.6, 0.07]
+    env_distance = (env_high - env_low) / pos_vel_state  # n_state = 50
     position_idx = int((state[0] - env_low[0]) / env_distance[0])
     velocity_idx = int((state[1] - env_low[1]) / env_distance[1])
-    state_idx = position_idx + velocity_idx*n_states
-    return grid_states[state_idx]
+    state_idx = position_idx + velocity_idx * pos_vel_state
+    print ("### state_idx:", state_idx)
+    print ("########## idx_to_state(env, state) ##########")
+    return state_idx
 
 def get_action(state_idx):
+    print ("########## get_action(state_idx) ##########")
+    print ("### state_idx:", state_idx)
+    print ("### q_table[int(state_idx)]:", q_table[state_idx])
+    print ("########## get_action(state_idx) ##########")
     return np.argmax(q_table[state_idx])
 
 def update_q_table(state, action, reward, next_state):
+    print ("########## update_q_table(state, action, reward, next_state) ##########")
+    print ("### q_table:", q_table)
     q_1 = q_table[state][action]
+    print ("### q_1:", q_1)
+
     q_2 = reward + gamma * max(q_table[next_state])
     q_table[state][action] += q_learning_rate * (q_2 - q_1)
+    print ("########## update_q_table(state, action, reward, next_state) ##########")
 
+def find_policy():
+    return q_table
 
 def main():
     env = gym.make('MountainCar-v0')
+    trajectories = conv_traj(env, pos_vel_state)
 
     for episode in range(500000):
         state = env.reset()
         score = 0
 
         while True:
-            # env.render()
-            state = idx_to_state(env, state, n_states)
-            action = get_action(q_table[state])
+            env.render()
+            print ("### state:", state)
+            state = idx_to_state(env, state)
+            print ("### next_state after idx_to_state:", state)
+                        
+            action = get_action([int(state)])
+            print ("### action:", action)
+
             next_state, _, done, _ = env.step(action)
-            next_state = idx_to_state(env, next_state, n_states)
+            print ("### next_state:", next_state)
+            print ("### done:", done)
+            
+            next_state = idx_to_state(env, next_state)
+            print ("### next_state after idx_to_state:", next_state)
 
             # (2500, 3, 2500)
-            irl_reward = maxent.maxent_irl(grid_states, n_actions, gamma, 
-                                trajectories, epochs, theta_learning_rate, env)         
+            irl_reward = maxent.maxent_irl(feature_matrix, n_actions, q_table, gamma, 
+                                trajectories, epochs, theta_learning_rate, env)  
+
             reward = irl_reward[state][action][next_state]
             update_q_table(state, action, reward, next_state)
 
             score += reward
             state = next_state
+
             
             if done:
                 break
