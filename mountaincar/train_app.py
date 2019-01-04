781b45470a316430ccab535492782ee2c4737d66
Seunghyun Lee <shlee21@postech.ac.kr>
2019-01-04 18:55:14 +0900

    Update train_app.py

--- mountaincar/train_app.py
+++ mountaincar/train_app.py
@@ -6,7 +6,6 @@ from cvxopt import solvers  # convex optimization library
 from algorithms.rlstep import q_learning  # get the Reinforcement learner
 import gym
 
-
 FEATURES = 50
 
 # (20, 100, 4)
@@ -23,7 +22,10 @@ class irlAgent:
             np.asarray(self.expertPolicy) - np.asarray(self.randomPolicy))  # norm of the diff in expert and random
         self.policiesFE = {
             self.randomT: self.randomPolicy}  # storing the policies and their respective t values in a dictionary
-        print("Expert - Random at the Start (t) :: ", self.randomT)
+
+        print("[ExpertPE - Random atPE] the Start (t) :: \n", self.randomT)
+        print("\n==========================================================================\n")
+
         self.currentT = self.randomT
         self.minimumT = self.randomT
         self.env = env
@@ -31,30 +33,39 @@ class irlAgent:
     def getRLAgentFE(self, W, i):  # get the feature expectations of a new poliicy using RL agent
 
         Qtable = q_learning(W, i)  # train the agent and save the model in a file used below
-
-        return calcNewFE(self.env, Qtable, W)  # return feature expectations by executing the learned policy
+        self.qtable = Qtable
+        return calcNewFE(self.env, Qtable, W) # return feature expectations by executing the learned policy
 
     def policyListUpdater(self, W, i):  # add the policyFE list and differences
-        tempFE = self.getRLAgentFE(W, i)  # get feature expectations of a new policy respective to the input weights
+        tempFE= self.getRLAgentFE(W, i)  # get feature expectations of a new policy respective to the input weights
         hyperDistance = np.abs(np.dot(W, np.asarray(self.expertPolicy) - np.asarray(tempFE)))  # hyperdistance = t
         self.policiesFE[hyperDistance] = tempFE
         return hyperDistance  # t = (weights.tanspose)*(expert-newPolicy)
 
     def optimalWeightFinder(self):
-        f = open('weights.txt', 'w')
+        f_w = open('weights.txt', 'w')
+        f_fe = open('featexp.txt', 'w')
         i = 1
         while True:
+            print("\n=============================:: Interation %d ::===========================\n"% (i))
+            print("Start QP Solver.")
             W = self.optimization()  # optimize to find new weights in the list of policies
-            print("weights ::", W)
-            f.write(str(W))
-            f.write('\n')
-            print("the distances  ::", self.policiesFE.keys())
+            print("\nWeights ::")
+            print(W,'\n')
+            f_w.write(str(W))
+            f_w.write('\n')
+            print("Distances (t) List ::\n", list(self.policiesFE.keys()), '\n')
             self.currentT = self.policyListUpdater(W, i)
-            print("Current distance (t) is:: ", self.currentT)
+            print("Current distance (t) is:: \n", self.currentT, '\n')
+            f_fe.write(str(list(self.policiesFE.values())))
+            f_fe.write('\n')
+            print("===========================================================================\n")
             if self.currentT <= self.epsilon:  # terminate if the point reached close enough
+                self.play()
                 break
             i += 1
-        f.close()
+        f_w.close()
+        f_fe.close()
         return W
 
     def optimization(self):  # implement the convex optimization, posed as an SVM problem
@@ -77,21 +88,52 @@ class irlAgent:
         weights = weights / norm
         return weights  # return the normalized weights
 
+    def play(self):
+
+        q_table = self.qtable
+
+        # Create a new game instance.
+        env = gym.make('MountainCar-v0')
+
+        state = env.reset()
+        score = 0
+
+        while True:
+            env.render()
+            state_idx = self.idx_to_state(env, state)
+            action = np.argmax(q_table[state_idx])
+            next_state, _, done, _ = env.step(action)
+            state = next_state
+            if done:
+                break
+
+    def idx_to_state(self, env, state):
+        env_low = env.observation_space.low
+        env_high = env.observation_space.high
+        env_distance = (env_high - env_low) / 50
+        position_idx = int((state[0] - env_low[0]) / env_distance[0])
+        velocity_idx = int((state[1] - env_low[1]) / env_distance[1])
+        state_idx = position_idx + velocity_idx * 50
+        return state_idx
+
 
 if __name__ == '__main__':
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
     env = gym.make('MountainCar-v0')
     trejectories = np.load(file="make_expert/expert_trajectories.npy")
 
+    print("\n============================:: Initialization ::==========================\n")
     randomPolicyFE = randomFE(FEATURES)
-    print("RandomFE ::", randomPolicyFE)
+    print("RandomFE ::")
+    print(randomPolicyFE, '\n')
     # ^the random policy feature expectations
     expertPolicyFE = expertFE(env, trejectories, FEATURES)
-    print("ExpertFE ::",expertPolicyFE)
+    print("ExpertFE ::")
+    print(expertPolicyFE, '\n')
     # ^feature expectations for the "follow Yellow obstacles" behavior
 
-    epsilon = 0.1
+    epsilon = 0.01
     irlearner = irlAgent(env, randomPolicyFE, expertPolicyFE, epsilon)
-    print("The Optimal Weight is ::",irlearner.optimalWeightFinder())
+
+    print("\nThe Optimal Weight is ::\n",irlearner.optimalWeightFinder())
+
 
