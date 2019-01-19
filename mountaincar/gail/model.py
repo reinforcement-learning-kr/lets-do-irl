c2e11d2ad0eb086723ae2148fc9e1cd2e4d5fb25
dongminleeai <kid33629@gmail.com>
2019-01-20 04:05:35 +0900

    ppo, gail completion

---
+++ mountaincar/gail/model.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+
+
+class Actor(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, num_outputs)
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
+
+    def forward(self, x):
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        policies = torch.softmax(self.fc3(x), dim=-1)
+        return policies
+
+
+class Critic(nn.Module):
+    def __init__(self, num_inputs):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 1)
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
+
+    def forward(self, x):
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        value = self.fc3(x)
+        return value
+
+
+class Discriminator(nn.Module):
+    def __init__(self, num_inputs):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 1)
+        self.fc1.weight.data.fill_(-1.0)
+        self.fc1.bias.data.fill_(-1.0)
+        self.fc3.weight.data.mul_(0.1)
+        self.fc3.bias.data.mul_(0.0)
+
+    def forward(self, x):
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        prob = torch.sigmoid(self.fc3(x))
+        return prob
