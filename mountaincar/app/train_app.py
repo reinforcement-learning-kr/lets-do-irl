13048ceba047bca3f3f582e622377969552428db
Yoon, Seungje <dbsdns@naver.com>
2019-02-05 12:51:28 +0900

    Add files via upload

--- mountaincar/app/train_app.py
+++ mountaincar/app/train_app.py
@@ -1,139 +1,233 @@
 import numpy as np
-import logging
-from algorithms.feature_expectation import calcNewFE, randomFE, expertFE
-from cvxopt import matrix
-from cvxopt import solvers  # convex optimization library
-from algorithms.rlstep import q_learning  # get the Reinforcement learner
 import gym
+import random
+import sys
+import cvxpy as cp
+import matplotlib.pyplot as plt
 
-FEATURES = 50
+N_idx = 20
+F_idx = 2
+GAMMA = 0.99
 
-# (20, 100, 4)
-trejectories = np.load(file="make_expert/expert_trajectories.npy")
+trejectories = np.load(file="expert_trajectories.npy")
 
 
-class irlAgent:
-    def __init__(self, env, randomFE, expertFE, epsilon):
-        self.randomPolicy = randomFE
-        self.expertPolicy = expertFE
+def idx_to_state(env, state):
+    env_low = env.observation_space.low
+    env_high = env.observation_space.high
+    env_distance = (env_high - env_low) / N_idx
+    position_idx = int((state[0] - env_low[0]) / env_distance[0])
+    velocity_idx = int((state[1] - env_low[1]) / env_distance[1])
+    state_idx = position_idx + velocity_idx * N_idx
+    return state_idx
 
-        self.epsilon = epsilon  # termination when t<0.1
-        self.randomT = np.linalg.norm(
-            np.asarray(self.expertPolicy) - np.asarray(self.randomPolicy))  # norm of the diff in expert and random
-        self.policiesFE = {
-            self.randomT: self.randomPolicy}  # storing the policies and their respective t values in a dictionary
-
-        print("[ExpertPE - Random atPE] the Start (t) :: \n", self.randomT)
-        print("\n==========================================================================\n")
-
-        self.currentT = self.randomT
-        self.minimumT = self.randomT
+class FeatureEstimate:
+    def __init__(self, env, num_features):
         self.env = env
+        self.num_features = num_features
+        self.feature = np.ones(self.num_features)
 
-    def getRLAgentFE(self, W, i):  # get the feature expectations of a new poliicy using RL agent
+    def gaussian(self, x, mu):
+        return np.exp(-np.power(x - mu, 2.) / (2 * np.power(1., 2.)))
 
-        Qtable = q_learning(W, i)  # train the agent and save the model in a file used below
-        self.qtable = Qtable
-        return calcNewFE(self.env, Qtable, W) # return feature expectations by executing the learned policy
+    def get_features(self, state):
+        env_low = self.env.observation_space.low
+        env_high = self.env.observation_space.high
+        env_distance = (env_high - env_low) / (self.num_features - 1)
 
-    def policyListUpdater(self, W, i):  # add the policyFE list and differences
-        tempFE= self.getRLAgentFE(W, i)  # get feature expectations of a new policy respective to the input weights
-        hyperDistance = np.abs(np.dot(W, np.asarray(self.expertPolicy) - np.asarray(tempFE)))  # hyperdistance = t
-        self.policiesFE[hyperDistance] = tempFE
-        return hyperDistance  # t = (weights.tanspose)*(expert-newPolicy)
+        for i in range(int(self.num_features/2)):
+            #position
+            self.feature[i] = self.gaussian(state[0], env_low[0] + i * env_distance[0])
+            #velocity
+            self.feature[i+int(self.num_features/2)] = self.gaussian(state[1], env_low[1] + i * env_distance[1])
 
-    def optimalWeightFinder(self):
-        f_w = open('weights.txt', 'w')
-        f_fe = open('featexp.txt', 'w')
-        i = 1
-        while True:
-            print("\n=============================:: Interation %d ::===========================\n"% (i))
-            print("Start QP Solver.")
-            W = self.optimization()  # optimize to find new weights in the list of policies
-            print("\nWeights ::")
-            print(W,'\n')
-            f_w.write(str(W))
-            f_w.write('\n')
-            print("Distances (t) List ::\n", list(self.policiesFE.keys()), '\n')
-            self.currentT = self.policyListUpdater(W, i)
-            print("Current distance (t) is:: \n", self.currentT, '\n')
-            f_fe.write(str(list(self.policiesFE.values())))
-            f_fe.write('\n')
-            print("===========================================================================\n")
-            if self.currentT <= self.epsilon:  # terminate if the point reached close enough
-                self.play()
-                break
-            i += 1
-        f_w.close()
-        f_fe.close()
-        return W
+        return self.feature
 
-    def optimization(self):  # implement the convex optimization, posed as an SVM problem
-        m = len(self.expertPolicy)
-        P = matrix(2.0 * np.eye(m), tc='d')  # min ||w||
-        q = matrix(np.zeros(m), tc='d')
-        policyList = [self.expertPolicy]
-        h_list = [1]
-        for i in self.policiesFE.keys():
-            policyList.append(self.policiesFE[i])
-            h_list.append(1)
-        policyMat = np.matrix(policyList)
-        policyMat[0] = -1 * policyMat[0]
-        G = matrix(policyMat, tc='d')
-        h = matrix(-np.array(h_list), tc='d')
-        sol = solvers.qp(P, q, G, h)
+def random_feature_expectation(num_features):
+    return np.random.normal(size=num_features)
 
-        weights = np.squeeze(np.asarray(sol['x']))
-        norm = np.linalg.norm(weights)
-        weights = weights / norm
-        return weights  # return the normalized weights
+def expert_feature_expectation(env, trajectories, num_features):
+    featureExpectations = np.zeros(num_features)
+    feature_estimate = FeatureEstimate(env, num_features)
+    for m in range(len(trajectories)):
+        for car_steps in range(len(trajectories[0])):
+            state = trajectories[m][car_steps]
+            features = feature_estimate.get_features(state)
+            featureExpectations += (GAMMA**(car_steps))*np.array(features)
+    featureExpectations = featureExpectations / len(trajectories)
+    return featureExpectations
 
-    def play(self):
+def QP_optimizer(expertList, agentList):
+    w=cp.Variable(F_idx)
+    
+    policyMat = agentList
+    ExpertMat = expertList
+        
+    constraints = [(ExpertMat-policyMat)*w >= 2]
+        
+    obj = cp.Minimize(cp.norm(w))
+    prob = cp.Problem(obj, constraints)
 
-        q_table = self.qtable
+    prob.solve()
 
-        # Create a new game instance.
-        env = gym.make('MountainCar-v0')
+    if prob.status == "optimal":
+        print("status:", prob.status)
+        print("optimal value", prob.value)
+    
+        weights = np.squeeze(np.asarray(w.value))
+        return weights, prob.status
 
+    else:
+        print("status:", prob.status)
+        
+        weights = np.zeros(4)
+        return weights, prob.status
+
+
+def add_policyList(policyList, feature_expectation):
+    #agent의 RL과정이 끝난 후 나온 FE를 list에 저장
+    policyList = np.vstack([policyList, feature_expectation])
+    return policyList
+
+def subtract_policyList(policyList):
+    #infeasible할시에 맨위 FE를 list에 제외
+    policyList = policyList[1:][:]
+    return policyList
+
+def calc_feature_expectation(env, qtable):
+    featureExpectations = np.zeros(F_idx)
+    feature_estimate = FeatureEstimate(env, F_idx)
+    scoreList = []
+    n_traj = 20
+    
+    for _ in range(n_traj):
+        state = env.reset()
+        car_steps = 0
+        score = 0
+        done = False
+        
+        while not done and (car_steps<=120):
+            car_steps += 1
+
+            # Choose action.
+            state_idx = idx_to_state(env, state)
+            action = (np.argmax(q_table[state_idx]))
+
+            # Take action.
+            next_state, r, done, _ = env.step(action)
+            # calculate FE
+            features = feature_estimate.get_features(next_state)
+            featureExpectations += (GAMMA**(car_steps))*np.array(features)
+
+            score+=r
+            state = next_state
+
+        scoreList.append(score)
+    
+    featureExpectations = featureExpectations/ n_traj
+    print("avg_score:", np.mean(scoreList))
+    print("\n")
+    print("current_feature_expectation:", featureExpectations)
+
+    return featureExpectations
+
+def plot_score(scores, epi):
+
+    fig,ax = plt.subplots(1,1,  figsize=(10, 10))
+    
+    plt.title('Scores in episode : {}'.format(epi))
+    
+    plt.plot(scores)
+
+    ax.set_ylabel('score')
+    ax.set_xlabel('episode')
+    
+    plt.draw()
+    plt.savefig('Scores in episode_{}.png'.format(epi))
+    plt.close(fig)
+
+if __name__ == '__main__':
+    print(":: Start Q-learning.\n")
+    gamma = 0.99
+    q_learning_rate = 0.03
+
+    n_states = N_idx**2  # position - 20, velocity - 20
+    n_actions = 3
+    q_table = np.zeros((n_states, n_actions))  # (400, 3)
+
+    # Create a new game instance.
+    env = gym.make('MountainCar-v0')
+    episode = 0
+    scores = []
+    feature_estimate = FeatureEstimate(env, F_idx)
+    
+    #초기화
+
+    print("\n============================:: Initialization ::==========================\n")
+    randomPolicy_feature_expectation = random_feature_expectation(F_idx)
+    print("Random_feature_expectation ::")
+    print(randomPolicy_feature_expectation, '\n')
+
+    expertPolicy_feature_expectation = expert_feature_expectation(env, trejectories, F_idx)
+    print("Expert_feature_expectation ::")
+    print(expertPolicy_feature_expectation, '\n')
+
+    expert_feature_expectation_List = np.matrix([expertPolicy_feature_expectation])
+    agent_feature_expectation_List = np.matrix([randomPolicy_feature_expectation])
+    
+    W, status = QP_optimizer(expert_feature_expectation_List, agent_feature_expectation_List)
+
+    while True:
         state = env.reset()
         score = 0
 
         while True:
-            env.render()
-            state_idx = self.idx_to_state(env, state)
+            state_idx = idx_to_state(env, state)
+
             action = np.argmax(q_table[state_idx])
-            next_state, _, done, _ = env.step(action)
+
+            next_state, reward, done, _ = env.step(action)
+            next_state_idx = idx_to_state(env, next_state)
+
+            features = feature_estimate.get_features(next_state)
+            irl_reward = np.dot(W, features)
+
+            # Update Q-table
+            q_1 = q_table[state_idx][action]
+            q_2 = reward + gamma * max(q_table[next_state_idx])
+            q_table[state_idx][action] += q_learning_rate * (q_2 - q_1)
+
+            score += reward
             state = next_state
+
             if done:
+                scores.append(score)
+                if episode >= 35000:#np.mean(scores[-min(10, len(scores)):]) > -120:
+                    print(":: TOUCH DOWN EPISODE %d / SCORE %d \n" % (episode, np.mean(scores[-min(10, len(scores)):])))
+                    np.save("best_q_table", arr=q_table)
+                    print(":: Complete Q-learning.\n")
+
+                    plot_score(scores, episode)
+                    
+                    env.close()
+                    sys.exit()
+
+                episode += 1
                 break
 
-    def idx_to_state(self, env, state):
-        env_low = env.observation_space.low
-        env_high = env.observation_space.high
-        env_distance = (env_high - env_low) / 50
-        position_idx = int((state[0] - env_low[0]) / env_distance[0])
-        velocity_idx = int((state[1] - env_low[1]) / env_distance[1])
-        state_idx = position_idx + velocity_idx * 50
-        return state_idx
+        if episode % 1000 == 0:
+            #1000ep마다 score 표시
+            print('{} episode | score: {:.1f}'.format(episode, np.mean(scores[-1000:])))
 
+        if episode % 5000 == 0:
+            #10000ep마다 weight optimize
 
-if __name__ == '__main__':
-    env = gym.make('MountainCar-v0')
-    trejectories = np.load(file="make_expert/expert_trajectories.npy")
-
-    print("\n============================:: Initialization ::==========================\n")
-    randomPolicyFE = randomFE(FEATURES)
-    print("RandomFE ::")
-    print(randomPolicyFE, '\n')
-    # ^the random policy feature expectations
-    expertPolicyFE = expertFE(env, trejectories, FEATURES)
-    print("ExpertFE ::")
-    print(expertPolicyFE, '\n')
-    # ^feature expectations for the "follow Yellow obstacles" behavior
-
-    epsilon = 0.01
-    irlearner = irlAgent(env, randomPolicyFE, expertPolicyFE, epsilon)
-
-    print("\nThe Optimal Weight is ::\n",irlearner.optimalWeightFinder())
-
+            status = "infeasible"
+            temp_feature_expectation = calc_feature_expectation(env, q_table)
+            agent_feature_expectation_List = add_policyList(agent_feature_expectation_List, temp_feature_expectation)
+            
+            while status=="infeasible":
+                W, status = QP_optimizer(expert_feature_expectation_List, agent_feature_expectation_List)
+                if status=="infeasible":
+                    agent_feature_expectation_List = subtract_policyList(agent_feature_expectation_List)
 
